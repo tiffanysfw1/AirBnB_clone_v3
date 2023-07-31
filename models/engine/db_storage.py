fae76801cf78825237fda22cfa6c8b31ef449eb8
tiffanysfw1 <tiffyjemohappy19@gmail.com>
2023-07-31 03:34:36 -0400

    tasks

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,18 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+               
+    def get(self, cls, id):
+        """Retrieve an object based on the class and ID."""
+        if cls and id:
+            key = cls.__name__ + '.' + id
+            return self.__session.query(cls).filter_by(id=id).first()
+        return None
+
+    def count(self, cls=None):
+        """Count the number of objects in storage matching the given class."""
+        if cls:
+            return self.__session.query(cls).count()
+        return self.__session.query(User).count() + self.__session.query(State).count() + \
+               self.__session.query(City).count() + self.__session.query(Amenity).count() + \
+               self.__session.query(Place).count() + self.__session.query(Review).count()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,14 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+               
+    def get(self, cls, id):
+        """Retrieve an object based on the class and ID."""
+        key = cls.__name__ + '.' + id
+        return self.__objects.get(key, None)
+
+    def count(self, cls=None):
+        """Count the number of objects in storage matching the given class."""
+        if cls:
+            return len(self.all(cls))
+        return len(self.__objects)
