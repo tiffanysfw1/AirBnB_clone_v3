557b257ac799615ee9c6b35ea0910043bc30f196
coder <122115043+tiffanysfw1@users.noreply.github.com>
2023-07-31 23:45:55 -0400

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,40 @@
-from flask import Flask, Blueprint, jsonify
-import os
-from api.v1.views import app_views
+#!/usr/bin/python3
+"""
+API for AirBnB_clone_v3
+"""
 
+import os
+from flask import Flask, jsonify, Response
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ handles teardown """
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """ handles 404 errors """
+    status = {"error": "Not found"}
+    return jsonify(status), 404
+
 
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port)
+    try:
+        host = os.environ.get('HBNB_API_HOST')
+    except:
+        host = '0.0.0.0'
 
+    try:
+        port = os.environ.get('HBNB_API_PORT')
+    except:
+        port = '5000'
+
+    app.run(host=host, port=port)
